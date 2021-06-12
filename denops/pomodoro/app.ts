080eb2fbb0e956168214fe0b53819d3102be9cc2
uki00a <uki00a@gmail.com>
2021-06-12 16:14:00 +0900

    feat: Basic support for PomodoroStart command

--- denops/pomodoro/app.ts
+++ denops/pomodoro/app.ts
@@ -1,5 +1,8 @@
 import { main } from "./deps.ts";
 import { createNotifier } from "./notifiers.ts";
+import { Pomodoro } from "./pomodoro.ts";
+import { createRenderer } from "./renderer.ts";
+import { createTimer } from "./timer.ts";
 
 main(async ({ vim }) => {
   vim.register({
@@ -9,9 +12,17 @@ main(async ({ vim }) => {
       const message = "Hello!";
       await notifier.notify(title, message);
     },
+    async start() {
+      const timer = createTimer();
+      const notifier = createNotifier();
+      const renderer = createRenderer(vim);
+      const pomodoro = new Pomodoro(timer, notifier, renderer);
+      await pomodoro.start();
+    },
   });
 
-  await vim.execute(
+  await vim.execute([
     `command! DenopsNotify call denops#notify("${vim.name}", "notify", [])`,
-  );
+    `command! PomodoroStart call denops#notify("${vim.name}", "start", [])`,
+  ]);
 });
--- denops/pomodoro/renderer.ts
+++ denops/pomodoro/renderer.ts
@@ -1,3 +1,29 @@
+import { Vim } from "./deps.ts";
+
 export interface Renderer {
   render(remaining: number): Promise<void>;
+}
+
+class VimRenderer implements Renderer {
+  #vim: Vim;
+
+  constructor(vim: Vim) {
+    this.#vim = vim;
+  }
+
+  async render(remaining: number): Promise<void> {
+    await this.#vim.g.set("pomodoro_timer_status", makeStatusline(remaining));
+  }
+}
+
+function makeStatusline(remaining: number): string {
+  const minutes = Math.floor((remaining / 1000) / 60);
+  const seconds = (remaining / 1000) % 60;
+  return `${String(minutes).padStart(2, "0")}:${
+    String(seconds).padStart(2, "0")
+  }`;
+}
+
+export function createRenderer(vim: Vim): Renderer {
+  return new VimRenderer(vim);
 }

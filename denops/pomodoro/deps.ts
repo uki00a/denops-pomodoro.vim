61f1c229777d78570846bffa1530e52cfac1fc05
uki00a <uki00a@gmail.com>
2021-06-20 22:48:04 +0900

    fix: Use setInterval to implement Timer

--- denops/pomodoro/deps.ts
+++ denops/pomodoro/deps.ts
@@ -1,2 +1,3 @@
 export { main, Vim } from "https://deno.land/x/denops_std@v0.13/mod.ts";
 export { readAll } from "https://deno.land/std@0.97.0/io/util.ts";
+export { deferred } from "https://deno.land/std@0.97.0/async/deferred.ts";
--- denops/pomodoro/timer.ts
+++ denops/pomodoro/timer.ts
@@ -1,28 +1,57 @@
+import { deferred } from "./deps.ts";
+
 export interface Timer {
   start(): AsyncIterableIterator<number>;
+  stop(): void;
 }
 
-function sleep(ms: number): Promise<void> {
-  return new Promise<void>((resolve) => {
-    setTimeout(resolve, ms);
-  });
+interface IteratorResult {
+  done: boolean;
+  value: number;
 }
 
 export function createTimer(
   duration = 25 * 60 * 1000,
   interval = 1000,
 ): Timer {
-  async function* start() {
+  let timerID: number | null = null;
+  function start() {
     let remaining = duration;
-    while (remaining > 0) {
-      yield remaining;
-      await sleep(interval);
-      remaining -= interval;
+    let currentPromise = deferred<IteratorResult>();
+
+    timerID = setInterval(() => {
+      if (remaining > 0) {
+        currentPromise.resolve({ value: remaining, done: false });
+        remaining -= interval;
+        currentPromise = deferred<IteratorResult>();
+      } else {
+        stop();
+        currentPromise.resolve({ done: true, value: remaining });
+      }
+    }, interval);
+
+    const iter = {
+      [Symbol.asyncIterator]() {
+        return iter;
+      },
+      next() {
+        return currentPromise;
+      },
+    };
+
+    return iter;
+  }
+
+  function stop(): void {
+    if (timerID) {
+      clearInterval(timerID);
+      timerID = null;
     }
   }
 
   const timer = {
     start,
+    stop,
   };
 
   return timer;

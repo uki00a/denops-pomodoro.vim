f72a31b31e479a18a0790d4d4af5ca619964abc7
uki00a <uki00a@gmail.com>
2021-07-04 16:47:19 +0900

    chore: Bump denops_std to v1.0.0-alpha.5

--- denops/pomodoro/deps.ts
+++ denops/pomodoro/deps.ts
@@ -1,7 +1,7 @@
-export type { Denops } from "https://deno.land/x/denops_std@v1.0.0-alpha.0/mod.ts";
-export type { VariableHelper } from "https://deno.land/x/denops_std@v1.0.0-alpha.0/variable/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v1.0.0-alpha.0/variable/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v1.0.0-alpha.0/helper/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v1.0.0-alpha.5/mod.ts";
+export type { VariableHelper } from "https://deno.land/x/denops_std@v1.0.0-alpha.5/variable/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v1.0.0-alpha.5/variable/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v1.0.0-alpha.5/helper/mod.ts";
 export {
   ensureNumber,
   ensureString,
